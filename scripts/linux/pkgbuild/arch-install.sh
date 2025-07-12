ea06c8ce5e10ea2209be26b95e3dfab20d744ba9
kasimeka <kasimeka@tuta.io>
2025-07-12 07:23:26 +0300

    update arch-install script to point to `v0.4.0`

--- scripts/linux/pkgbuild/arch-install.sh
+++ scripts/linux/pkgbuild/arch-install.sh
@@ -11,7 +11,7 @@ trap 'rm -rf "$PKGBUILD_DIR"' EXIT
 cat <<'EOF' > PKGBUILD
 # Maintainer: kasimeka
 pkgname=bromomethane
-pkgver=0.3.0
+pkgver=0.4.0
 pkgrel=1
 pkgdesc="A mod manager for Balatro - easily install and manage mods for the popular roguelike deckbuilding game. (none)"
 arch=('x86_64')
@@ -21,8 +21,8 @@ groups=()
 depends=('cairo' 'desktop-file-utils' 'gdk-pixbuf2' 'glib2' 'gtk3' 'hicolor-icon-theme' 'libsoup3' 'openssl' 'pango' 'webkit2gtk-4.1')
 options=('!strip' '!emptydirs')
 install=${pkgname}.install
-source_x86_64=("https://github.com/kasimeka/bromomethane/releases/download/v0.3.0/bromomethane_0.3.0_amd64.deb")
-sha512sums_x86_64=('570010ed88241e640e31544d6d1bb30a25950ecb3c39adfc7a49886b7e495ddf88523d2bc13f075a83e3982ee975479dc6fdae85a656e9229a69936527f6ecc8')
+source_x86_64=("https://github.com/kasimeka/bromomethane/releases/download/v0.4.0/bromomethane_0.4.0_amd64.deb")
+sha512sums_x86_64=('ee3d923bad5816f7ab5cef6c63eb86b5e9694efd78f48e7e77e75857def79796993ab605357b3487ac0608f6d67cd33eeccfbeaf9dd60112d38c7091101241f8')
 
 package(){
 	tar -xz -f data.tar.gz -C "${pkgdir}"
